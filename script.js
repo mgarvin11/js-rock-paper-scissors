33594cad88f3eced033c4e87015ca747d2624695
mgarvin11 <mgarvin11@gmail.com>
2024-07-13 16:34:32 -0400

    Rock, Paper, Scissor Revised

--- script.js
+++ script.js
@@ -1,68 +1,67 @@
 // computer play thatis randomly generated ech time called
-    function computerplay() {
-        let rock = "Rock";
-        let paper = "Paper";
-        let scissors = "Scissors";
-        let getRandomValue = Math.random();
-        if (getRandomValue <= 0.33) {
-            return rock;
-        } else if (getRandomValue <= 0.66) {
-            return paper;
-        } else {
-            return scissors;
-        }
+function computerplay() {
+    let rock = "Rock";
+    let paper = "Paper";
+    let scissors = "Scissors";
+    let getRandomValue = Math.random();
+    if (getRandomValue <= 0.33) {
+        return rock;
+    } else if (getRandomValue <= 0.66) {
+        return paper;
+    } else {
+        return scissors;
     }
+}
     
-    //Game start
-    function game() {
-        let playerWin = 0;
-        let computerWin = 0;
-        let gameWinner = "";
+//Game start
+function game() {
+    let playerWin = 0;
+    let computerWin = 0;
+    let gameWinner = "";
 
-        //Add Event Listeners
-        const buttons = document.querySelectorAll('button');
-        buttons.forEach((button) => {
-            buttons.addEventListener('click', () => {
-                playerSelection = button.className;
-                const computerSelection = computerplay();
-                battleWinText.textcontent = (playRound(playerSelection, computerSelection))
-                player.WinText.textContent = "Player Win Totals: " + playerWin;
-                computer.WinText.textContent = "Computer Win totals: " + computerWin;
-            })
+    //Add Event Listeners
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            playerSelection = button.className;
+            const computerSelection = computerplay();
+            battleWinText.textcontent = (playRound(playerSelection, computerSelection));
+            playerWinText.textContent = "Player Win Totals: " + playerWin;
+            computerWinText.textContent = "Computer Win totals: " + computerWin;
+            endGame();
         })
+    })
 
-            //play the round and determine winner
-            function playRound(firstLetterCap, computerSelection) {
-                let tie = "It's a Tie! You selected " + firstLetterCap + " and the computer selected " + computerSelection;
-                let paperBeatRock = "You Win! You selected " + firstLetterCap + " and the computer selected " + computerSelection;
-                let scissorsBeatPaperLoss = "You Lose! You selected " + firstLetterCap + " and the computer selected " + computerSelection;
-                let paperBeatRockLoss = "You Lose! You selected " + firstLetterCap + " and the computer selected " + computerSelection;
-                let rockBeatScissors = "You Win! You selected " + firstLetterCap + " and the computer selected " + computerSelection;
-                let rockBeatScissorsLoss = "You Lose! You selected " + firstLetterCap + " and the computer selected " + computerSelection;
-                let scissorsBeatPaper = "You Win! You selected " + firstLetterCap + " and the computer selected " + computerSelection;
+        //play the round and determine winner
+        function playRound(playerSelection, computerSelection) {
+            let tie = "It's a Tie! You selected " + playerSelection + " and the computer selected " + computerSelection + ".";
+            let paperBeatRock = "You Win! You selected " + playerSelection + " and the computer selected " + computerSelection + ".";
+            let scissorsBeatPaperLoss = "You Lose! You selected " + playerSelection + " and the computer selected " + computerSelection + ".";
+            let paperBeatRockLoss = "You Lose! You selected " + playerSelection + " and the computer selected " + computerSelection + ".";
+            let rockBeatScissors = "You Win! You selected " + playerSelection + " and the computer selected " + computerSelection + ".";
+            let rockBeatScissorsLoss = "You Lose! You selected " + playerSelection + " and the computer selected " + computerSelection + ".";
+            let scissorsBeatPaper = "You Win! You selected " + playerSelection + " and the computer selected " + computerSelection + ".";
     
-
-                if (firstLetterCap === computerSelection) {
-                    return tie;
-                } else if ((firstLetterCap === "Paper") && (computerSelection === "Rock")) {
-                    playerWin++;
-                    return paperBeatRock;
-                } else if ((firstLetterCap === "Paper") && (computerSelection === "Scissors")) {
-                    computerWin++;
-                    return scissorsBeatPaperLoss;
-                } else if ((firstLetterCap === "Rock") && (computerSelection === "Paper")) {
-                    computerWin++;
-                    return paperBeatRockLoss;
-                } else if ((firstLetterCap === "Rock") && (computerSelection === "Scissors")) {
-                    playerWin++;
-                    return rockBeatScissors;
-                } else if ((firstLetterCap === "Scissors") && (computerSelection === "Rock")) {
-                    computerWin++;
-                    return rockBeatScissorsLoss;
-                } else if ((firstLetterCap === "Scissors") && (computerSelection === "Rock")) {
-                    computerWin++;
-                    return scissorsBeatPaper;
-                }
+            if (playerSelection === computerSelection) {
+                return tie;
+            } else if ((playerSelection === "Paper") && (computerSelection === "Rock")) {
+                playerWin++;
+                return paperBeatRock;
+            } else if ((playerSelection === "Paper") && (computerSelection === "Scissors")) {
+                computerWin++;
+                return scissorsBeatPaperLoss;
+            } else if ((playerSelection === "Rock") && (computerSelection === "Paper")) {
+                computerWin++;
+                return paperBeatRockLoss;
+            } else if ((playerSelection === "Rock") && (computerSelection === "Scissors")) {
+                playerWin++;
+                return rockBeatScissors;
+            } else if ((playerSelection === "Scissors") && (computerSelection === "Rock")) {
+                computerWin++;
+                return rockBeatScissorsLoss;
+            } else if ((playerSelection === "Scissors") && (computerSelection === "Rock")) {
+                computerWin++;
+                return scissorsBeatPaper;
             }
         }
 
@@ -75,13 +74,13 @@
     // create player win tracking DOM
     const playerWinText = document.createElement("p");
     playerWinText.style.color = "blue";
-    playerWinText.textContent = "PLayer Win Totals: " + playerWin;
+    playerWinText.textContent = "Player Win Totals: " + playerWin;
     resultsDiv.appendChild(playerWinText);
 
     // create computer win tracking DOM
     const computerWinText = document.createElement("p");
     computerWinText.style.color = "blue";
-    computerWinText.textcontent = "Compueter Win Totals: " + computerWin;
+    computerWinText.textcontent = "Computer Win Totals: " + computerWin;
     resultsDiv.appendChild(computerWinText);
 
     // create battle win text DOM
@@ -135,6 +134,8 @@
             })
         }
     }
+}
 
-
+// function call to start the game
+game();
 
